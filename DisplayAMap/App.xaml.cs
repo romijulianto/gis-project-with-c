2f60387256a13ccf0c52c351fa4bc868c9598eae
Romi Julianto <romyjulians@gmail.com>
2022-10-13 15:46:15 +0700

    Add project files.

---
+++ DisplayAMap/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DisplayAMap
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = "AAPKfcc16db8d5734a4a93d43963468cbe18M4yUrWq7MnBN-RKuk7i_E9vJg2ZDNoNWruuRtfkLeNCpCuaUo8g4Ggjt6-8FvpDj";
+        }
+    }
+}
