67702c15a8f5f417369623eba250f8f7d4347b53
Romi Julianto <romyjulians@gmail.com>
2022-10-13 15:59:36 +0700

    add MapViewModel

---
+++ DisplayAMap/MapViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DisplayAMap
+{
+    class MapViewModel : INotifyPropertyChanged
+    {
+        // Add a constructor to the class that calls SetupMap when a new MapViewModel is instantiated
+        public MapViewModel()
+        {
+            SetupMap();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // Define a new property on the view model called Map
+        private Map _map;
+        public Map Map
+        {
+            get { return _map; }
+            set
+            {
+                _map = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // Create function to the MapViewModel class called SetupMap
+        private void SetupMap()
+        {
+            // Create a new map with a 'topographic vector' basemap.
+            Map = new Map(BasemapStyle.ArcGISTopographic);
+        }
+    }
+}
